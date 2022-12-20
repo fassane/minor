a0c3408f86bd23c27111f14be88666ae1fde4d58
fassane <fassanebolly@gmail.com>
2022-12-20 02:59:10 +0000

    index file

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import AppLogoFooter from "./AppLogoFooter";
+import AppLogoHeader from "./AppLogoFooter";
+import AppModal from "./AppModal";
+import ImagesContainer from "./ImagesContainer";
+
+export {
+    AppLogoFooter,
+    AppLogoHeader,
+    AppModal,
+    ImagesContainer,
+    
+}
